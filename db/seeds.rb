cc58322afab0147e23c808488a3570ba66834e77
Joris Lespagnol <lespagnol.joris@gmail.com>
2019-03-13 17:13:53 +0100

    exo 2 front-end

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+Review.destroy_all
+
+puts "Begin Seed"
+
+  5.times do
+    restaurant = Restaurant.create!(
+      name: Faker::Movies::BackToTheFuture.character,
+      address: Faker::Address.street_address,
+      category: Restaurant::CATEGORIES.sample,
+    )
+    5.times do
+      Review.create!(
+        content: Faker::TvShows::SouthPark.quote,
+        rating: [0, 1, 2 , 3, 4, 5].sample,
+        restaurant: restaurant
+      )
+    end
+  end
+
+
+puts "Finished"
